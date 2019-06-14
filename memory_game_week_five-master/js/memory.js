635a532c6905981aec6432158a5792d619167cbe
Tomer Malovani <tomermalovani@gmail.com>
2019-06-14 17:59:06 +0300

    class is getting better

--- memory_game_week_five-master/js/memory.js
+++ memory_game_week_five-master/js/memory.js
@@ -1,26 +1,46 @@
 class Card {
-    constructor(id,imgUrl,clickd){
+    constructor(id, imgUrl, clickd) {
         this.id = id;
         this.imgUrl = imgUrl;
         this.clickd = clickd;
-        
+
     }
+
+    flipcard() {
+
+
+        this.id.addEventListener('click', function () {
+            
+                    this.classList.add("flip");
+                    let a = switch_card_back.bind(this);
+                    setTimeout(a,1000);
+
+        });
+
+    }
+     switch_card_back(){
+        this.classList.remove("flip");
+         }
+    
 }
 
-let newcardtoshowperzi = new Card("","","");
+
+let newcardtoshowperzi = new Card("", "", "");
 
 let cardsarr = [];
-for ( let i = 0; i < 11; i++){
-    cardsarr[i] = new Card (document.getElementById("card"+i),"",false);
-    //cardsarr[i] = document.getElementById("card"+i);
+for (let i = 0; i < 11; i++) {
+    cardsarr[i] = new Card(document.getElementById("card" + i), "", false);
+    cardsarr[i].flipcard();
+
 }
+
 
 
 // let cards = document.getElementsByClassName("card");
 // for(var i =0;i < cards.length;i++){
 //     cards[i].id = "card"+i;
 //     cards[i] = document.getElementById("card"+i);
-    
+
 // }
 // let card0 = document.getElementById("card0");
 
@@ -29,15 +49,12 @@ for ( let i = 0; i < 11; i++){
 // for(var i =0;i < cards.length;i++){
 // cards[i].addEventListener('click',switch_card);
 // }
-// function switch_card(){
-//     this.classList.add("flip");
-//     let a = switch_card_back.bind(this);
-//     setTimeout(a,1000);
+// 
 // }
-// function switch_card_back(){
-//     this.classList.remove("flip");
-//      }
- 
+function switch_card_back(){
+    this.classList.remove("flip");
+     }
+
 
 
 
