b1600190fbce7ecce11c44b1d529c2ab8342d7df
Tomer Malovani <tomermalovani@gmail.com>
2019-06-15 10:28:59 +0300

    shuffle is now working

--- memory_game_week_five-master/js/memory.js
+++ memory_game_week_five-master/js/memory.js
@@ -1,8 +1,9 @@
 class Card {
-    constructor(id, imgUrl, clickd) {
+    constructor(id,backid, imgUrl, clickd) {
         this.id = id;
         this.imgUrl = imgUrl;
         this.clickd = clickd;
+        this.backid = backid;
 
     }
 
@@ -12,27 +13,49 @@ class Card {
         this.id.addEventListener('click', function () {
             
                     this.classList.add("flip");
-                    let a = switch_card_back.bind(this);
-                    setTimeout(a,1000);
+                    // let a = switch_card_back.bind(this);
+                    // setTimeout(a,1000);
 
         });
 
     }
-     switch_card_back(){
-        this.classList.remove("flip");
-         }
+    //  switch_card_back(){
+    //     this.classList.remove("flip");
+    //      }
+
+    rand_img(imgarraycheck){
+        let imgarray = ["ca1","ca2","ca3","ca4","ca5","ca6","ca1","ca2","ca3","ca4","ca5","ca6"];
+        
+        let rand = Math.floor(Math.random() * 12);
+        if (imgarraycheck[rand]){
+            this.imgUrl = imgarray[rand];
+            this.put_img(rand,imgarray);
+            imgarraycheck[rand] = false;
+        }
+        else{
+            this.rand_img(imgarraycheck);
+        }
+    }
+
+    put_img(rand,imgarray){
+        this.backid.style.backgroundImage =`url("./img/${imgarray[rand]}.jpg")`;
+    }
     
 }
 
-
-let newcardtoshowperzi = new Card("", "", "");
+let imgarraycheck = [true,true,true,true,true,true,true,true,true,true,true,true,true];    
 
 let cardsarr = [];
-for (let i = 0; i < 11; i++) {
-    cardsarr[i] = new Card(document.getElementById("card" + i), "", false);
+for (let i = 0; i <= 11; i++) {
+    cardsarr[i] = new Card(document.getElementById("card" + i),document.getElementById("backcard" + i), "", false);
     cardsarr[i].flipcard();
+    cardsarr[i].rand_img(imgarraycheck);
 
 }
+
+
+
+
 
 
 
@@ -51,9 +74,9 @@ for (let i = 0; i < 11; i++) {
 // }
 // 
 // }
-function switch_card_back(){
-    this.classList.remove("flip");
-     }
+// function switch_card_back(){
+//     this.classList.remove("flip");
+//      }
 
 
 
